17d2fba641c850179fff38f78f58c26b51111ba0
Marco Alija Agostini <marukoalitini@gmail.com>
2025-10-18 19:55:22 -0300

    feat: adicionar respostas para as pergunstas da atividade

--- lru.py
+++ lru.py
@@ -15,8 +15,18 @@ def lru(sequencia: list[int], tamanho: int):
                 del ultimo_uso[lru_pagina]
         ultimo_uso[pagina] = tempo
         print(f"Passo {tempo + 1}: Página {pagina}, Memória: {memoria}")
+    print(f"Faltas: {faltas}")
+    return memoria
 
 
-sequencia = [4, 3, 25, 8, 19, 6, 25, 8, 16, 35, 45, 22, 8, 3, 16, 25, 7]
-tamanho = 8
-lru(sequencia, tamanho)
+if __name__ == "__main__":
+    sequencia = [4, 3, 25, 8, 19, 6, 25, 8, 16, 35, 45, 22, 8, 3, 16, 25, 7]
+    tamanho = 8
+    memorias = lru(sequencia, tamanho)
+    print(f"A página 7 está no quadro: {memorias.index(7) + 1}")
+    sequencia = [4, 5, 7, 9, 46, 45, 14, 4, 64, 7, 65, 2, 1, 6, 8, 45, 14, 11]
+    memorias = lru(sequencia, tamanho)
+    print(f"A página 11 está no quadro: {memorias.index(11) + 1}")
+    sequencia = [4, 6, 7, 8, 1, 6, 10, 15, 16, 4, 2, 1, 4, 6, 12, 15, 16, 11]
+    memorias = lru(sequencia, tamanho)
+    print(f"A página 11 está no quadro: {memorias.index(11) + 1}")
