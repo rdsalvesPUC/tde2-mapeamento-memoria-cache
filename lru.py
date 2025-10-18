efe713c405c73f3ee68cf87b9fabee2df5b5af83
Marco Alija Agostini <marukoalitini@gmail.com>
2025-10-18 19:28:50 -0300

    feat: implementar lógica do algoritmo LRU para gerenciamento de páginas

--- lru.py
+++ lru.py
@@ -3,3 +3,16 @@ tamanho = 8
 memoria = []
 faltas = 0
 ultimo_uso = {}
+
+for tempo in range(len(sequencia)):
+    pagina = sequencia[tempo]
+    if pagina not in memoria:
+        faltas += 1
+        if len(memoria) < tamanho:
+            memoria.append(pagina)
+        else:
+            lru_pagina = min(ultimo_uso, key=ultimo_uso.get)
+            memoria[memoria.index(lru_pagina)] = pagina
+            del ultimo_uso[lru_pagina]
+    ultimo_uso[pagina] = tempo
+    print(f"Passo {tempo + 1}: Página {pagina}, Memória: {memoria}")
