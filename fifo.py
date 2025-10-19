d47b122c1909e5a41a8cc2520e4e13da2a8c6796
rdsalves <s.rodrigo5@pucpr.edu.br>
2025-10-19 16:26:29 -0300

    Finalizado, tudo funcionando, testado com os 3 conjuntos de dados

--- fifo.py
+++ fifo.py
@@ -2,19 +2,37 @@ dados_a = [4,3,25,8,19,6,25,8,16,35,45,22,8,3,16,25,7]
 dados_b = [4,5,7,9,46,45,14,4,64,7,65,2,1,6,8,45,14,11]
 dados_c = [4,6,7,8,1,6,10,15,16,4,2,1,4,6,12,15,16,11]
 
+reg_posicao = 0
 qtd_paginas = 8
-memoria_vazia = []
-pf_contador = 0
+pf_contador = 0	
 
-def fifo(dados, memoria, paginas, pf):
+def fifo(dados, memoria, paginas, pf, registrador):
 	for i in dados:
-		if len(memoria) < paginas:
-			if i not in memoria: 
-				memoria.append(i)
+		if i not in memoria:
+			if registrador < paginas:
+				if len(memoria) < paginas:
+					memoria.append(i)
+					pf += 1
+					registrador += 1
+				else:
+					memoria[registrador] = i
+					pf += 1
+					registrador += 1
+			else: 
+				registrador = 0
+				memoria[registrador] = i
 				pf += 1
-			else:
-				print ("dado já alocado")
-	return print(memoria, pf)
+				registrador +=1
+		else:
+			print ("dado já alocado")
 
-fifo(dados_a, memoria_vazia, qtd_paginas, pf_contador)
+	return print(f'\nResultado: {memoria}, \nTotal Page Fault: {pf}, \nValor alvo na Página: {registrador}\n')
+
+fifo(dados_a, [], qtd_paginas, pf_contador, reg_posicao)
+fifo(dados_b, [], qtd_paginas, pf_contador, reg_posicao)
+fifo(dados_c, [], qtd_paginas, pf_contador, reg_posicao)
+
+
+
+
 	 
